b55abf7bbcd846cd2dc4d7570ababdaccc33e005
Zak Richards <zakofzaaz@gmail.com>
2025-02-21 15:56:08 +0000

    Changed db

--- myproject/myproject/myproject/settings.py
+++ myproject/myproject/myproject/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'soapdb.sqlite3',
+        'NAME': BASE_DIR / 'zakdb.sqlite3',
     }
 }
 
