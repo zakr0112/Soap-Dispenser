efd5386dca403879abf023c0ff905613d448e2ad
Zak Richards <zakofzaaz@gmail.com>
2025-02-28 15:48:03 +0000

    Updated dispenser view
    Updated the way that the model is loaded into the page

--- soap_project/dispenser/views.py
+++ soap_project/dispenser/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from .models import SoapDispenser
+from django.db import connection
+
+
 
 def home(request):
-    dispensers = SoapDispenser.objects.all()
-    return render(request, 'dispenser/home.html', {'dispensers': dispensers})
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT name, soap_level, last_refill FROM dispenser")
+        soaps = cursor.fetchall()
+
+    return render(request, 'dispenser/home.html', {'dispensers': soaps})
