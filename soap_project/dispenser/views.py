549f7e0da15874dc8c6e0740140e0d68e0e280c9
Zak Richards <zakofzaaz@gmail.com>
2025-03-06 17:06:38 +0000

    Update data cursor

--- soap_project/dispenser/views.py
+++ soap_project/dispenser/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .models import SoapDispenser
+from .models import Dispenser
 from django.db import connection
 
 
 
 def home(request):
     with connection.cursor() as cursor:
-        cursor.execute("SELECT name, soap_level, last_refill FROM dispenser")
+        cursor.execute("SELECT Dispenser_ID, Room_ID, Dis_Content, Level_Liquid FROM dispenser")
         soaps = cursor.fetchall()
 
     return render(request, 'dispenser/home.html', {'dispensers': soaps})
