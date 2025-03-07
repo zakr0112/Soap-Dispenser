2ab9c75500a2ae624c29f4361bcc174184f47122
Zak Richards <zakofzaaz@gmail.com>
2025-03-07 00:12:36 +0000

    Updated views for basic data

--- soap_project/dispenser/views.py
+++ soap_project/dispenser/views.py
@@ -10,3 +10,28 @@ def home(request):
         soaps = cursor.fetchall()
 
     return render(request, 'dispenser/home.html', {'dispensers': soaps})
+
+
+def dispenser_list(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT Dispenser_ID, Room_ID, Dis_Content, Level_Liquid FROM dispenser")
+        soaps = cursor.fetchall()
+    return render(request, 'dispenser/DispenserList.html', {'dispensers': soaps})
+
+def shifts(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT Dispenser_ID, Room_ID, Dis_Content, Level_Liquid FROM dispenser")
+        soaps = cursor.fetchall()
+    return render(request, 'dispenser/shifts.html', {'dispensers': soaps})
+
+def stock(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT Dispenser_ID, Room_ID, Dis_Content, Level_Liquid FROM dispenser")
+        soaps = cursor.fetchall()
+    return render(request, 'dispenser/stock.html', {'dispensers': soaps})
+
+def sites(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT Dispenser_ID, Room_ID, Dis_Content, Level_Liquid FROM dispenser")
+        soaps = cursor.fetchall()
+    return render(request, 'dispenser/sites.html', {'dispensers': soaps})
