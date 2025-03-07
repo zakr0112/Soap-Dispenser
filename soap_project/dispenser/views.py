001c08647f32f13dc387a793afffdfd6da3bab9a
Zak Richards <zakofzaaz@gmail.com>
2025-03-07 01:15:49 +0000

    Major change in views
    Made it so certain data is only retrievable by a certain action, makes no sense to explain but essentially company and restock ID's for the company restocking data table are seperate so it can show ONLY two companies (or as many as there are), not just by the row count

--- soap_project/dispenser/views.py
+++ soap_project/dispenser/views.py
@@ -20,15 +20,26 @@ def dispenser_list(request):
 
 def shifts(request):
     with connection.cursor() as cursor:
-        cursor.execute("SELECT Dispenser_ID, Room_ID, Dis_Content, Level_Liquid FROM dispenser")
+        cursor.execute("SELECT SHIFT_ID, Cleaner_ID, Start_Time, Finish_Time FROM SHIFT")
         soaps = cursor.fetchall()
     return render(request, 'dispenser/shifts.html', {'dispensers': soaps})
 
 def stock(request):
     with connection.cursor() as cursor:
-        cursor.execute("SELECT Dispenser_ID, Room_ID, Dis_Content, Level_Liquid FROM dispenser")
+        cursor.execute("SELECT Restock_ID, Supervisor_ID, Soap_Amount_Bought, Sanitizer_Amount_Bought, Soap_Price, Sanitizer_Price, Total, Company, Delivery_Date FROM RESTOCK")
         soaps = cursor.fetchall()
-    return render(request, 'dispenser/stock.html', {'dispensers': soaps})
+        companies_dict = {}
+
+    for soap in soaps:
+        company = soap[7]  
+        restock_id = soap[0] 
+
+        if company not in companies_dict:
+            companies_dict[company] = []
+
+        companies_dict[company].append(restock_id)
+
+    return render(request, 'dispenser/stock.html', {'companies_dict': companies_dict, 'dispensers': soaps})
 
 def sites(request):
     with connection.cursor() as cursor:
