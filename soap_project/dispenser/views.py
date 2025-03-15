7e78f42d86560d405d6031a43afb44347d08cf8c
Mark Richards <zaazapps@gmail.com>
2025-03-15 18:54:11 +0000

    Added DispenserResolved view
    Added get collection data and a lot of pagination

--- soap_project/dispenser/views.py
+++ soap_project/dispenser/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from .models import Dispenser
 from django.db import connection
+from .models import DispenserResolved
+from django.http import JsonResponse
+from datetime import datetime
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 
@@ -16,12 +20,15 @@ def dispenser_list(request):
     with connection.cursor() as cursor:
         cursor.execute("SELECT Dispenser_ID, Room_ID, Dis_Content, Level_Liquid FROM dispenser")
         soaps = cursor.fetchall()
+
     return render(request, 'dispenser/DispenserList.html', {'dispensers': soaps})
+
 
 def shifts(request):
     with connection.cursor() as cursor:
         cursor.execute("SELECT SHIFT_ID, Cleaner_ID, Start_Time, Finish_Time FROM SHIFT")
         soaps = cursor.fetchall()
+
     return render(request, 'dispenser/shifts.html', {'dispensers': soaps})
 
 def stock(request):
@@ -31,8 +38,8 @@ def stock(request):
         companies_dict = {}
 
     for soap in soaps:
-        company = soap[7]  
-        restock_id = soap[0] 
+        company = soap[7]
+        restock_id = soap[0]
 
         if company not in companies_dict:
             companies_dict[company] = []
@@ -48,4 +55,37 @@ def sites(request):
 
     with connection.cursor() as cursor:
         cursor.execute("SELECT Building_ID, Site_ID, Floors, Address, Phone_Number, Email FROM BUILDING")
+
     return render(request, 'dispenser/sites.html', {'dispensers': soaps})
+
+def dispenser_resolved(request):
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT * FROM vwDispenserResolved")
+        soaps = cursor.fetchall()
+
+    if not soaps:  # Check if empty
+        return render(request, 'dispenser/DispenserList.html', {'dispensers': None})
+
+    paginator = Paginator(soaps, 10)
+    page_number = request.GET.get('page', 1)
+
+    try:
+        page_obj = paginator.page(page_number)
+    except PageNotAnInteger:
+        page_obj = paginator.page(1)
+    except EmptyPage:
+        page_obj = paginator.page(paginator.num_pages)
+
+    return render(request, 'dispenser/DispenserList.html', {'page_obj': page_obj})
+
+def get_collection_data(request, dispenser_id):
+   with connection.cursor() as cursor:
+      cursor.execute("SELECT COLLECTION_ID, formatted_timedate, liquidMl FROM vwCollectionWithFormatting WHERE Dispenser_ID = %s LIMIT 10", [dispenser_id])
+      collection_data = cursor.fetchall()
+
+   # Convert to dictionary format for JSON response
+   collection_list = [
+      {"collection_id": row[0], "timedate": row[1], "level": row[2]}
+      for row in collection_data
+   ]
+   return JsonResponse(collection_list, safe=False)
