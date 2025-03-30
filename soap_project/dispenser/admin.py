5dd2afdb8f734a6e427ad0ec9ad5c73198041566
Mark Richards <zaazapps@gmail.com>
2025-03-30 19:18:17 +0100

    Added models to admin view

--- soap_project/dispenser/admin.py
+++ soap_project/dispenser/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+admin.site.register(Building)
+admin.site.register(Room)
+admin.site.register(Dispenser)
+admin.site.register(DispenserResolved)
+admin.site.register(Stockroom)
+admin.site.register(Supervisor)
+admin.site.register(Restock)
+admin.site.register(RoomRestock)
+admin.site.register(Rota)
+admin.site.register(Shift)
+admin.site.register(Collection)
