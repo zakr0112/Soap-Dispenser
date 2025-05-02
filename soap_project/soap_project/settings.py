8e0d3e43c2dcf1fd3a6b05a80d13b61234bfe269
zakr0112 <Z.Richards9969@student.leedsbeckett.ac.uk>
2025-05-02 15:08:16 +0100

    Updated settings for static delivery

--- soap_project/soap_project/settings.py
+++ soap_project/soap_project/settings.py
@@ -125,7 +125,11 @@ STATICFILES_DIRS = [
 ]
 
 if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
